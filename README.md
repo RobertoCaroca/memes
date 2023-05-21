9753c04cabdb0091f36aab6caeedbcdcd207410a
Roberto Caroca <63612984+RobertoCaroca@users.noreply.github.com>
2023-05-20 22:48:31 -0400

    New functionalities and Readme

---
+++ README.md
@@ -0,0 +1,24 @@
+Meme Generator
+
+This is a Meme Generator built using React and Bootstrap. It fetches meme images from the Imgflip API and allows users to search through the memes, download them, copy their URLs to the clipboard, and toggle their favorites.
+
+## Features
+
+- View a gallery of memes fetched from the Imgflip API.
+- Search through the meme gallery.
+- Toggle dark mode.
+- Download any meme image.
+- Copy the URL of any meme to the clipboard.
+- Add any meme to your favorites and view them later.
+- View memes by categories: Favorites, Popular, Latest.
+
+## Dependencies
+
+- [React](https://reactjs.org/)
+- [Bootstrap](https://getbootstrap.com/)
+- [Axios](https://axios-http.com/)
+- [Babel Standalone](https://babeljs.io/)
+
+## Contributing
+
+Feel free to fork this project, change it according to your needs and give us feedback.
--- memes.jsx
+++ memes.jsx
@@ -1,82 +1,103 @@
 const { useState, useEffect } = React;
 
 const Memes = () => {
-    const [memes, setMemes] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [favorites, setFavorites] = useState(
-        JSON.parse(localStorage.getItem('favorites')) || []
-    );
-    const [darkMode, setDarkMode] = useState(false);
+  const [memes, setMemes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem('favorites')) || []
+  );
+  const [darkMode, setDarkMode] = useState(false);
 
-    useEffect(() => {
-        axios.get('https://api.imgflip.com/get_memes')
-            .then(response => {
-                setMemes(response.data.data.memes);
-            })
-            .catch(error => console.log(error));
-    }, []);
+  useEffect(() => {
+    axios.get('https://api.imgflip.com/get_memes')
+      .then(response => {
+        setMemes(response.data.data.memes);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem('favorites', JSON.stringify(favorites));
-    }, [favorites]);
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }, [favorites]);
 
-    useEffect(() => {
-        document.body.classList.toggle('dark-mode', darkMode);
-    }, [darkMode]);
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
 
-    const filteredMemes = memes.filter(meme =>
-        meme.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  const copyToClipboard = (url) => {
+    navigator.clipboard.writeText(url);
+  }
 
-    const toggleFavorite = meme => {
-        if (favorites.some(favorite => favorite.id === meme.id)) {
-            setFavorites(favorites.filter(favorite => favorite.id !== meme.id));
-        } else {
-            setFavorites([...favorites, meme]);
-        }
-    };
+  const filteredMemes = memes.filter(meme =>
+    meme.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    return (
-        <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Meme Generator</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <input
-                    className="form-control"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                    onChange={event => setSearchTerm(event.target.value)}
-                  />
-                </li>
-              </ul>
-              <button className="btn btn-outline-success my-2 my-sm-0" onClick={() => setDarkMode(!darkMode)}>
-                Toggle Dark Mode
-              </button>
-            </div>
-          </nav>
-    
-          <div className="memes">
-            {filteredMemes.map((meme, index) => (
-                <div key={meme.id} className="meme">
-                <h2>{meme.name}</h2>
-                <img src={meme.url} alt={meme.name} />
-                <button className="btn btn-outline-primary" onClick={() => toggleFavorite(meme)}>
-                    {favorites.some(favorite => favorite.id === meme.id) 
-                        ? <i className="fas fa-heart"></i> 
-                        : <i className="far fa-heart"></i>
-                    }
-                </button> 
-              </div>
-            ))}
-          </div>
+  const toggleFavorite = meme => {
+    if (favorites.some(favorite => favorite.id === meme.id)) {
+      setFavorites(favorites.filter(favorite => favorite.id !== meme.id));
+    } else {
+      setFavorites([...favorites, meme]);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Meme Generator</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#favorites">Favorites</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#popular">Popular</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#latest">Latest</a>
+            </li>
+            <li className="nav-item">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                onChange={event => setSearchTerm(event.target.value)}
+              />
+            </li>
+          </ul>
+          <button className="btn btn-outline-success my-2 my-sm-0" onClick={() => setDarkMode(!darkMode)}>
+            Toggle Dark Mode
+          </button>
         </div>
-      );
-    };
-    
-    ReactDOM.render(<Memes />, document.getElementById('root'));
+      </nav>
+
+      <div className="memes">
+        {filteredMemes.map((meme, index) => (
+          <div key={meme.id} className="meme">
+            <h2>{meme.name}</h2>
+            <img src={meme.url} alt={meme.name} />
+            <button className="btn btn-outline-primary" onClick={() => toggleFavorite(meme)}>
+              {favorites.some(favorite => favorite.id === meme.id) 
+                ? <i className="fas fa-heart"></i> 
+                : <i className="far fa-heart"></i>
+              }
+            </button> 
+            <button className="btn btn-outline-primary">
+              <a href={meme.url} download>
+                <i className="fas fa-download"></i>
+              </a>
+            </button>
+            <button className="btn btn-outline-primary" onClick={() => copyToClipboard(meme.url)}>
+              <i className="fas fa-copy"></i>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Memes />, document.getElementById('root'));
